725ce4afdd131c38957509d4879edf43ff6048c5
Tonmoy <tonmoymondol2614@gmail.com>
2024-02-02 12:19:43 +0600

    call the function

---
+++ function.js
@@ -0,0 +1,14 @@
+// function declaration 
+function startFan(){
+    console.log ('stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+// call the function
+startFan();
+console.log('waking up in the morning')
+startFan();
+console.log('Eating lunch')
+console.log('watching JS tutorial')
+startFan();
